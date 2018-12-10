2714fffa1972a1a52b451e2496b06ab688e2ff86
Douglas G. Moore <doug@dglmoore.com>
2018-12-09 17:15:19 -0700

    Calculate σ1, σ2

--- assets/poincare.js
+++ assets/poincare.js
@@ -1,15 +1,21 @@
 /* global d3, Plotly */
 const { ipcRenderer } = require('electron');
-const plot = function({ sweep_y, sweep_y_label }, div='plot') {
+
+const stddev = function(data) {
+    const μ = data.reduce((acc, x) => acc + x) / data.length;
+    return Math.sqrt(data.reduce((acc, x) => acc + (x - μ)**2) / (data.length - 1));
+};
+
+const plot = function({ x, y, label }, div='plot') {
     const trace = {
-        x: sweep_y.slice(0, sweep_y.length-1),
-        y: sweep_y.slice(1),
+        x: x,
+        y: y,
         mode: 'markers',
         type: 'scattergl'
     };
 
     const layout = {
-        title: `Poincaré Plot: (${sweep_y_label})`,
+        title: `Poincaré Plot: (${label})`,
         xaxis: { title: 'xₜ' },
         yaxis: { title: 'xₜ₊₁'},
         showLegend: false,
@@ -25,6 +31,38 @@ const plot = function({ sweep_y, sweep_y_label }, div='plot') {
     });
 };
 
-ipcRenderer.on('data', function(event, data) {
-    plot(data);
+const deviations = function(x, y) {
+    const root_2 = Math.sqrt(2);
+
+    const data = new Array(x.length);
+    for (let i = 0, len = data.length; i < len; ++i) {
+        data[i] = (y[i] - x[i]) / root_2;
+    }
+    const σ1 = stddev(data);
+
+    for (let i = 0, len = data.length; i < len; ++i) {
+        data[i] = (x[i] + y[i]) / root_2;
+    }
+    const σ2 = stddev(data);
+
+    return { σ1, σ2 };
+};
+
+ipcRenderer.on('data', function(event, { sweep_y, sweep_y_label }) {
+    const x = sweep_y.slice(0, sweep_y.length - 1);
+    const y = sweep_y.slice(1);
+
+    const { σ1, σ2 } = deviations(x, y);
+
+    d3.select('#sigma1').attr('value', σ1.toFixed(3));
+    d3.select('#sigma2').attr('value', σ2.toFixed(3));
+    d3.select('#sigma12').attr('value', (σ1/σ2).toFixed(3));
+
+    d3.select('#loading').remove();
+    d3.select('#data').style('display', 'flex');
+    plot({
+        x: x,
+        y: y,
+        label: sweep_y_label
+    });
 });
